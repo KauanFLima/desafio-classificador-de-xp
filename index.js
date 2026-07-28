2a45ad1f2c27f54e45243c62990956b5ac6eed95
KauanFLima <lordragebs@gmail.com>
2026-07-28 19:41:08 -0400

    Projeto Finalizado

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ else if (nivelDeXp >=5001 && nivelDeXp <=7000){nivelDePatente = "Ouro"}
 else if(nivelDeXp >=7001 && nivelDeXp <=8000){nivelDePatente = "Platina"}
 else if(nivelDeXp >=8001 && nivelDeXp <=9000){nivelDePatente = "Ascendente"}
 else if(nivelDeXp >=9001 && nivelDeXp <=10000){nivelDePatente = "Imortal"}
-else if(nivelDeXp >=10001){nivelDePatente= "Radiante"}
+else if(nivelDeXp >=10001){nivelDePatente = "Radiante"}
 
 console.log ("O Herói de nome " + nome + " está no nível de " + nivelDePatente)
